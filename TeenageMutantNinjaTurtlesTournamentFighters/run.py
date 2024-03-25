5d629cbc4c000b70cab72d98d0df35f24e1c7bff
tiansongyu <tiansongyu@directdrivetech.com>
2024-03-25 16:34:48 +0800

    feat: Add ninja turtle fight.
    Signed-off-by: tiansongyu <tiansongyu@directdrivetech.com>

---
+++ TeenageMutantNinjaTurtlesTournamentFighters/run.py
@@ -0,0 +1,99 @@
+# Copyright 2023 invoker__qq. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import os
+import time 
+
+import retro
+from stable_baselines3 import PPO
+
+from ninja_turtles_fight_wrapper import TeenageMutantNinjaTurtlesTournamentFighters
+
+RESET_ROUND = True  # Whether to reset the round when fight is over. 
+RENDERING = True    # Whether to render the game screen.
+
+MODEL_NAME = r"ppo_ryu_4687500_steps" 
+
+RANDOM_ACTION = False
+NUM_EPISODES = 30 # Make sure NUM_EPISODES >= 3 if you set RESET_ROUND to False to see the whole final stage game.
+MODEL_DIR = r"trained_models/"
+
+def make_env(game, state):
+    def _init():
+        env = retro.make(
+            game=game, 
+            state=state, 
+            use_restricted_actions=retro.Actions.FILTERED,
+            obs_type=retro.Observations.IMAGE
+        )
+        env = TeenageMutantNinjaTurtlesTournamentFighters(env, reset_round=RESET_ROUND, rendering=RENDERING)
+        return env
+    return _init
+
+game = "TeenageMutantNinjaTurtlesTournamentFighters-Nes"
+env = make_env(game, state="Level1.LeoVsRaph.Tournament")()
+
+if not RANDOM_ACTION:
+    model = PPO.load(os.path.join(MODEL_DIR, MODEL_NAME), env=env)
+
+obs = env.reset()
+done = False
+
+num_episodes = NUM_EPISODES
+episode_reward_sum = 0
+num_victory = 0
+
+print("\nFighting Begins!\n")
+
+for _ in range(num_episodes):
+    done = False
+    
+    if RESET_ROUND:
+        obs = env.reset()
+
+    total_reward = 0
+
+    while not done:
+        timestamp = time.time()
+
+        if RANDOM_ACTION:
+            obs, reward, done, info = env.step(env.action_space.sample())
+        else:
+            action, _states = model.predict(obs)
+            obs, reward, done, info = env.step(action)
+
+        if reward != 0:
+            total_reward += reward
+            print("Reward: {:.3f}, playerHP: {}, enemyHP:{}".format(reward, info['health'], info['enemy_health']))
+        
+        if info['enemy_health'] < 1 or info['health'] < 1:
+            done = True
+
+    if info['enemy_health'] < 1:
+        print("Victory!")
+        num_victory += 1
+
+    print("Total reward: {}\n".format(total_reward))
+    episode_reward_sum += total_reward
+
+    if not RESET_ROUND:
+        while info['enemy_health'] < 1 or info['health'] < 1:
+        # Inter scene transition. Do nothing.
+            obs, reward, done, info = env.step([0] * 12)
+            env.render()
+
+env.close()
+print("Winning rate: {}".format(1.0 * num_victory / num_episodes))
+if RANDOM_ACTION:
+    print("Average reward for random action: {}".format(episode_reward_sum/num_episodes))
+else:
+    print("Average reward for {}: {}".format(MODEL_NAME, episode_reward_sum/num_episodes))
